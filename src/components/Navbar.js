331456387d4e25d35a575d75a6b1fe43bd8c09ac
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-21 23:59:23 -0300

    isActive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
 export default function Navbar() {
   return (
-      <>
+    <>
       <ul>
-          <li><NavLink to="/" >Home</NavLink></li>
-          <li><NavLink to="/about" >About</NavLink></li>
-          <li><NavLink to="/users" >Users</NavLink></li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "blue")}
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "blue")}
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "blue")}
+            to="/users"
+          >
+            Users
+          </NavLink>
+        </li>
       </ul>
-          
-          
-          
-      
-      </>
-  )
+    </>
+  );
 }
